3adbc1406a5c10256e7869d84fb6f9e296c834d8
Henrik Wilhelm Sissener <hws902@gmail.com>
2018-02-21 00:37:17 +0100

    Add files via upload

--- Angular_modules/GitHub-Profile/script.js
+++ Angular_modules/GitHub-Profile/script.js
@@ -5,14 +5,27 @@
     var MainController = function($scope, $http) {
         var onUserComplete = function(response) {
             $scope.user = response.data;
-        }
-        var onError = function(reason) {
-            $scope.error = "could not fetch the user."
+            $http.get($scope.user.repos_url)
+                .then(onRepos, onError);
         };
-        $http.get("https://api.github.com/users/wisihe")
-            .then(onUserComplete);
-        $scope.message = "Hello, Angular!";
+        var onRepos = function(response) {
+            $scope.repos = response.data;
+        };
+
+        var onError = function(reason) {
+            $scope.error = "could not fetch data."
+        };
+
+        $scope.search = function(username) {
+            $http.get("https://api.github.com/users/" + username)
+                .then(onUserComplete);
+        }
+
+
+        $scope.username = "wisihe";
+        $scope.message = "Github Viewer";
+        $scope.repoSortOrder = "-stargazers_count";
 
     };
-    app.controller("MainController",["$scope","$http",  MainController]);
+    app.controller("MainController", ["$scope", "$http", MainController]);
 }());
